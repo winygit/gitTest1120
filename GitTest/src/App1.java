745a032491a420e8a15d9caceece35f43b9e7957
winy <jiant.wu@hnair.com>
2016-11-20 20:24:19 +0800

    1

---
+++ GitTest/src/App1.java
@@ -0,0 +1,11 @@
+
+public class App1 {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+        System.out.println("hello");
+        System.out.println("lxx!");
+    }
+
+}
---
+++ GitTest/src/App2.java
@@ -0,0 +1,12 @@
+import java.util.Date;
+
+public class App2 {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+        System.out.println("/**************/");
+        System.out.println(new Date().toLocaleString());
+    }
+
+}
